76a9e7b7daba3b8a2402fc80e2c0facdc0701ec8
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-02-21 11:34:35 +0300

    redis connection

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,13 +1,21 @@
+using Basket.API.Entities;
+
 namespace Basket.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            ShoppingCart shoppingCart = new();
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddStackExchangeRedisCache(q => 
+            {
+                //q.Configuration = Environment.GetEnvironmentVariable("RedisConnection");
+                q.Configuration = "localhost:6379";
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
