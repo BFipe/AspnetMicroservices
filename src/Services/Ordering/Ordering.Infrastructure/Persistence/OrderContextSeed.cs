3d48017eecd3d8b251bf223eaa223676057d5368
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-03-16 13:36:58 +0300

    Order repository

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (orderContext.Orders.Any() == false)
+            {
+                await orderContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Prebuilded seeded data sucessfully added to database");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Ilya", LastName = "Ilyusha", EmailAddress = "shilomylo@gmail.com", AddressLine = "Minsk", Country = "Belarus", TotalPrice = 69420 }
+            };
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+    {
+        private readonly OrderContext _dbContext;
+        private readonly ILogger<RepositoryBase<T>> _logger;
+
+        public RepositoryBase(OrderContext dbContext, ILogger<RepositoryBase<T>> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
